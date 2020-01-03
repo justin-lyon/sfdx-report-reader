0bdafe64d2add383ceb454d6032cb1b96ea62b49
Justin Lyon <jml6487@gmail.com>
2020-01-03 14:06:40 -0600

    init commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# report-reader
+
+> A Node CLI command to read a Report Folder from a Sandbox alias and print out the Reports by FullName <FolderName/ReportName>
+
+## Usage
+
+```bash
+# Clone the repo
+git clone <repo url>
+
+# change directory to the repo
+cd report-reader
+
+# run the command
+# output > src/reportNames.log
+npm start my-sandbox-alias target-report-folder
+```
---
+++ src/index.js
@@ -0,0 +1,55 @@
+const args = process.argv.slice(2)
+if (!alias) throw new Error('First argument should be a sandbox alias. ex: `npm start <my-alias>`')
+if (!folderName) throw new Error('Second argument should be a folder api name. ex: `npm start <my-alias> <my-folder-name>`')
+
+const { spawn } = require('child_process')
+const fs = require('fs')
+const path = require('path')
+// const reports = require('./reports')
+const output = path.join('src', 'reportNames.log')
+
+const alias = args[0]
+const folderName = args[1]
+
+const mdapiArgs = [ 'force:mdapi:listmetadata', '-u', alias, '-m', 'Report', '--folder', folderName, '--json' ]
+const options = {
+  cwd: path.join(__dirname, '..'),
+  env: process.env,
+  shell: true
+}
+
+const burpStream = stream => {
+  return new Promise((resolve, reject) => {
+    const buffer = []
+    stream
+      .on('data', data => {
+        buffer.push(data)
+      })
+      .on('end', () => {
+        resolve(buffer.join('').toString().trim())
+      })
+      .on('error', err => {
+        reject(err)
+      })
+  })
+}
+
+const main = () => {
+  const listMetadata = spawn('sfdx', mdapiArgs, options)
+  burpStream(listMetadata.stdout)
+    .then(data => {
+      const reports = JSON.parse(data).result
+      // console.log(reports)
+      console.log(typeof reports)
+      const fullNames = reports.map(r => r.fullName)
+      console.log(fullNames)
+      fs.writeFile(output, fullNames.join('\n'), err => {
+        if (err) console.error('Error writing to file: ', err)
+      })
+    })
+    .catch(err => {
+      console.error('Error executing sfdx force:mdapi:listmetadata', mdapiArgs, err)
+    })
+}
+
+main()
